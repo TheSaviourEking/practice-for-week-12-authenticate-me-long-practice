55bb310c1d603b2f1f9ca1e38c3d8a66a49f639b
TheSaviourEking <saviour.eking@outlook.com>
2024-03-21 16:23:50 +0100

    chore: signup validation and error handlers

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -5,13 +5,35 @@ const router = express.Router();
 const { User } = require('../../db/models');
 const { setTokenCookie } = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
-    const { email, password, username } = req.body;
-    const user = await User.signup({ email, username, password });
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation.js');
 
-    await setTokenCookie(res, user);
+const validateSignup = [
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+]
 
-    return res.json({ user });
-});
+router.post(
+    '/',
+    validateSignup,
+    async (req, res) => {
+        const { email, password, username } = req.body;
+        const user = await User.signup({ email, username, password });
+
+        await setTokenCookie(res, user);
+
+        return res.json({ user });
+    });
 
 module.exports = router;
